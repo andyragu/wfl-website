cc00da5749a59520de5c0a83ee799aa84dddccb6
andyragu <114187135+andyragu@users.noreply.github.com>
2025-08-27 17:14:06 -0400

    about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,6 +3,19 @@ export default function About(){
     return (
       <main className = "min-h-screen bg-[#EEEEEE]">
         <h1 className="header">About the WFL</h1>
+        <div className="text-box-1">
+          <p>
+            The WFL is a fantasy football league born during the height of the
+            COVID-19 pandemic in 2021, forged through nightly Call of Duty: Warzone
+            sessions among friends. The Haaretz has said "we're a vibrant and rich universe".
+          </p>
+          <p>
+            Originally composed of 10 members, the league had a merger with the WBWA
+            to include 12 teams. The roster of managers includes: Johnny, Drew, Jack, Ale,
+            Fabian, Jere, Eric, Daniel, Andres, Mesa, Parker, Andy, and Jonah.
+          </p>
+        </div>
+        <h1 className="header-1">Hall of Champions</h1>
       </main>
     )
   }
