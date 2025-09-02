c4d1f8f2e30cc6464062f0ce6ca938332fbf91cf
andyragu <114187135+andyragu@users.noreply.github.com>
2025-09-02 10:31:22 -0400

    taskbar change

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -4,7 +4,7 @@ import logo from './media/logo.svg';
 export default function NavBar() {
     return (
         <nav className="flex justify-between items-center px-6 py-4 bg-[#ffffff] border-b border-gray-300">
-        <div className="text-xl text-[#A23131] font-bold font-sans">WFL</div>
+        <div className="text-xl text-[#A23131] font-bold font-sans"><Link href="/">WFL</Link></div>
         <ul className="flex gap-4 text-[#A23131] font-sans font-bold">
             <li><Link href="/">Home</Link></li>
             <li><Link href="/about">About</Link></li>
