70d70a22059a52cdc48f470485744a402d71c409
drewbloss <drew_bloss@CommishHQ.localdomain>
2025-09-12 19:44:38 -0400

    Bug Fixes

--- src/app/articles/wfl-weekly-2/page.tsx
+++ src/app/articles/wfl-weekly-2/page.tsx
@@ -1,8 +1,6 @@
 import "../styles.css"
 
-import "../styles.css"
-
-export default function WFLWeekly1(){
+export default function WFLWeekly2(){
     return (
         <main className = "min-h-screen bg-[#ffffff]">
             <div className="header">
@@ -17,49 +15,37 @@ export default function WFLWeekly1(){
                         <p>{'Fantasy football = life, so by using the transitive property, fantasy football is also all about the ups and downs, etc. etc. And there is arguably no greater representation of this, than Week 1.'} </p>
                         <p>{'Point totals were up and down, and GMs went from riding high to feeling plenty low in an instant. Do you overreact and blow it up? Stay the course and let it ride? Week 1 often creates so much questions than it does answers, and it’s up to you to decide your path forward. Perhaps WFL Weekly can provide an insight into just what the GMs are thinking about this week. The fireworks surrounding the WFL’s opening week festivities have concluded, and it’s time to get to work.'}</p>
                         <p>{`Without further ado… let’s rock n roll`}</p>
-                        <br></br>
                         <h2>{`Biggest Winner`}</h2>
-                        <br></br>
                         <p>{'The Bang Bros (Andres)'}</p>
                         <p>{'A breakthrough year with their first playoff appearance in history, gave way to a bombshell dropped in the offseason with Mesa taking a leave of absence effective immediately. The Bang Bros name remained the same, but their office felt a lot emptier than it had been in quite some time. A draft that had many asking questions led to a playoff rematch with the reigning WFL Champion in Jack who spoiled their playoff debut last season. And right on Sunday night when everything was looking bleak and Derrick Henry had seemingly just sealed the deal, Josh Allen and Keon Coleman went to work. They scored, scored again, and continued to score all the way towards bringing the margin all the way down to 2.5 points. And on Monday Night, TJ Hockenson took care of the rest. It’s an incredible moment for a team that has waded through uncertainty up until this point, and makes you wonder just how far this Andres-led team could go.'}</p>
                         <p>{'We had the chance to sit down with the GM of The Bang Bros and ask him about what the victory meant to him after everything that’s happened, and what we can expect moving forward from his team. He told us "You’re absolutely right. It’s always terrible for the bros to be banging alone especially this season as democracy burns before us. Like any normal, healthy man, you need two to be able to tango and create magic. Unfortunately I lost a bro but this victory means that any man can survive on that 1 nut energy. I can expect and pray for Hailee and J Allen to ride each other as they will hopefully ride me to the championship again this year."'}</p>
                         <p>{' A true poet. Up next for the original Bang Bro is a date with Came.'}</p>
-                        <br></br>
                             <h3>{'Other Winners'}</h3>
                             <p>{'This is a call for help (Parker)'}</p>
                             <p>{'Following a disastrous 2024 season, and losing the WFL Commissioner election, it would’ve been easy and understandable if Parker and his team just didn’t have the juice in Week 1 against a division rival that he shares a lot of history with. Instead, This is a call for help smashed Worse than WHL, scoring an astounding 159.18 points to take a convincing win into Week 2. Puka Nacua, Zay Flowers, and Bijan Robinson all led the way with 20+ point efforts, and solid performances elsewhere made up for a dud from Terry McLaurin.'}</p>
                             <p>{'All of a sudden Parker’s team could be better than we all thought with an opportunity to climb to 2-0 against Golden Boys this week in a massive divisional clash.'}</p>
-                            <br></br>
                             <p>{'Travis Kelce (Player on Hall of Fame Youtuber)'}</p>
                             <p>{'Fresh off his engagement to global icon Taylor Swift, Kelce was caught hitting on another man… HARD. So hard in fact, he dislocated the man’s shoulder. We’re of course talking about Kelce pulling out a WWE move and taking out his teammate Xavier Worthy on Kansas City’s opening drive of the game. Last season was Kelce’s worst since his rookie year statistically, so injuring your competition for targets is certainly a way to improve your numbers and make fantasy owners happy.'}</p>
                             <p>{'Unless you own Xavier Worthy and your name is Alejandro. It almost feels like we’ve seen this somewhere before…'}</p>
-                            <br></br>
                         <h2>{'Biggest Loser'}</h2>
                         <p>{'Came (Daniel)'}</p>
                         <p>{'We’re going to try and be gracious with Daniel and his team this season given the circumstances and the expectations being quite low, but if Week One was any indication, it is going to be a long season for Came. Simply put, almost nothing went right for Daniel’s squad. 3 players in double figures, no kicker started, and an inactive Darnell Mooney in the starting lineup all added up to an abysmal 77.12 points. While it won’t always be that bad, and Came won’t end up here too often as long as things don’t get much worse, any hope of Daniel’s squad being a surprise dark horse is not looking right now. The 1st ever WFL Commissioner has turned water into wine before, so if there’s anyone who can pull something out and make his team respectable it’s him, but this is truly his most daunting task yet.'}</p>                        
                         <p>{'If you’re sleeping on this team, don’t worry, so is he.'}</p>
-                        <br></br>
                             <h3>{'Other Losers'}</h3>
                             <p>{'Marvin’s Room (Jack)'}</p>
                             <p>{'The reigning WFL champion looked to be picking up right where he left off last season halfway through Sunday Night Football, as Derrick Henry had Marvin’s Room with the highest point total in the league. And at the end of the week, there was only one team that had a higher point total than Jack’s squad. Unfortunately for him, that just so happened to be his opponent. As we’ve detailed above in the Biggest Winner section, it was a historic comeback that sent this team to a shocking 0-1 and spoiled what was looking to be almost a victory lap for the 2 time champ. Jack’s teams have historically had some crushing losses against Andres-led teams, and this was undoubtedly the worst of all. After the game was over, our reporter asked Jack for comment on the game during his postgame presser, to which all he said was "It doesn’t feel good to lose. We’re on to Hall of Fame Youtuber."'}</p>
                             <p>{'Pure business as usual.'}</p>
-                            <br></br>
                             <p>{'Wake and Baker Co. (Jonah)'}</p>
                             <p>{'All was looking up for Wake and Baker Co. through the first quarter of Monday Night Football. A tough back and forth matchup with Take us to Valhalla Lamar saw D\'Andre Swift seemingly well on his way to delivering the finishing blow to Andy, and sending Jonah to Week 2 with a 1-0 record. Andddd then Ben Johnson kind of forgor how to use him, the o-line fell apart, and so did the Bears, as Wake and Baker Co. lost by an agonizing 2.68 point margin. In a week where points were hard to come by, scoring 148 points is pretty solid, but the luck of the draw was not in Jonah’s favor this time around.'}</p>
                             <p>{'Jonah’s teams have a history of losing razor thin close games and costing him later down the road; will that be the case again this season?'}</p>
-                            <br></br>
                             <p>{'Jewish WFL GMs'}</p>
                             <p>{'Both Jewish WFL GMs lost in heartbreaking fashion this week to teams ran by an Andres. VERY interesting… 🧐'}</p>
-                            <br></br>
                         <h2>{'Donuts! (Sponsored by Tough Cookie Inc.)'}</h2>
                         <p>{'The famed WFL Bakery is BACK! We’re ramping up, and ready to give you all the wonderfully delicious treats you have a love/hate relationship with!'}</p>
-                        <br></br>
                         <p>{'🍩 Darnell Mooney (Came)'}</p>
                         <p>{'Stop me if you’ve heard this before: Daniel oversleeps and starts an injured & inactive player. Hey at least it wasn’t a playoff game this time!'}</p>
-                        <br></br>
                         <p>{'🍩 Xavier Worthy (Edna Mode & Guest)'}</p>
                         <p>{'In yet another Déjà vu moment, Ale watched his Chiefs WR1 get taken out by his own teammate. And incredibly enough that teammate, Travis Kelce, also happens to be on Fabian’s team. The crash out was warranted.'}</p>
-                        <br></br>
                         <h2>{'Overreactions & Predictions:'}</h2>
                         <p>{'With a tumultuous preseason, WFL Draft, and Week 1 all in the rear view mirror, it’s time for one of the WFL’s favorite traditions… the predictions! We’ve got 2 or 3 from every GM in the league, including some Mesa predictions sprinkled in! Happy reading!'}</p>
                         <ul>
@@ -96,11 +82,9 @@ export default function WFLWeekly1(){
                             <h3>{'WFL Game of the Week: KRAFT YAC & CHEESE (Drew) vs. Take us to Valhalla Lamar (Andy)'}</h3>
                             <p>{'It’s Week 2 and already we have an absolute barnburner on our hands. Two proud and passionate GMs that have had their fair share of success across all fantasy leagues, meet in a clash of eras. The league’s former Commissioner, Drew, is free from league politics following a rocky end to his tenure last season. With a new fire in his gut, he’s looking to make a statement this week against his successor and told us "Frankly, I’ve always thought of Andy as a fantasy fraud. He got lucky drafting triple crown winner Cooper Kupp in the third round his first season. He traded away all his players after a disappointing week 1 and finished dead last in 2024. But now he expects me to give him respect because he got a loaded team off the back of Daniel’s shenanigans and proclaimed himself dictator of the WFL? Nah. I may be the underdog but I’ll be relishing this victory and I can’t wait to beat him again in Week 13."'}</p>
                             <p>{'The current WFL Commissioner, Andy, might not have the years of experience his predecessor has, but he’s no stranger to the limelight having reached two WFL Championship Finals, along with a WFL Punishment. He aims to silence his critics this week and simply told us "I’m excited for the matchup. Many are saying that I’m already a better commissioner than he is. It should be a nice test. Commish division seems like a cake walk." The temperature with this matchup is at an all-time high, and it shows no signs of cooling off. Will Drew stay undefeated in the Cummish Division at 2-0? Or will Andy keep the former commissioner at bay in the WFL Game of the Week?'}</p>
-                            <br></br>
                             <h3>{'Upset Watch of the Week: Golden Boys (Johnny) vs This is a call for help (Parker)'}</h3>
                             <p>{'Talk about a grudge match, these two have got to be chomping at the bit for Week 2 to kick off. While on paper, Parker’s squad certainly is favored, we’re not so sure we trust them as the team in second place just yet. Questions at QB, TE, and FLEX could provide an opening for Johnny to sneak away with a W. His team is bound for positive regression, and if anyone is gonna will their team to a win this week, bet on Johnny.'}</p>
-                            <p>{'We caught up with the two fierce rivals earlier to discuss their matchup, and Parker told us "I love facing Johnny. It’s always a fun matchup between the 2 of us and I can’t to see this one again between the classic rivalry." Johnny was unwavering with his response, telling us that "My only thought against my division 'rival' is to win. I know my projections have me losing but when you go against parker you always gotta have a little confidence going into a matchup against him." Will Johnny provide a rude awakening and prove that Parker’s Week One was just a fluke? Or will Parker send his division rival back to the drawing board 0-2?'}</p>
-                            <br></br>
+                            <p>{'We caught up with the two fierce rivals earlier to discuss their matchup, and Parker told us "I love facing Johnny. It’s always a fun matchup between the 2 of us and I can’t to see this one again between the classic rivalry." Johnny was unwavering with his response, telling us that "My only thought against my division \'rival\' is to win. I know my projections have me losing but when you go against parker you always gotta have a little confidence going into a matchup against him." Will Johnny provide a rude awakening and prove that Parker’s Week One was just a fluke? Or will Parker send his division rival back to the drawing board 0-2?'}</p>
                         
                         <h3>{`Predictions: (Last Week’s results: 3-3 🥸)`}</h3>
                         <ul>
@@ -111,16 +95,16 @@ export default function WFLWeekly1(){
                             <li>{'#7 Marvin’s Room (Jack) ✅ vs. #5 Hall of Fame Youtuber (Fabian)'}</li>
                             <li>{'#4 KRAFT YAC & CHEESE (Drew) vs. #3 Take us to Valhalla Lamar (Andy) ✅'}</li>
                         </ul>
-                        <br></br>
+
                         <p style={{ fontWeight: "bold" }}>{'A big shoutout to our longest running sponsor Tough Cookie Inc. and Ms. Taylor Collins for partnering with WFL Weekly once more for the always wonderful Donut Section! Wouldn’t be the same without ya!'}</p>
-                        <br></br>
+
                         <p>{'If you or someone you know would like to be a sponsor for WFL Weekly, let us know and we’ll give you some options on where you can showcase your brand, company, or organization!'}</p>
-                        <br></br>
+
                         <p>{'Additionally, if you have your own ideas for columns, one offs, or weekly installments, speak to Chief Editor Jack Reid for more information!'}</p>
-                        <br></br>
-                        <p style={{ fontWeight: "italic" }}>{'The only good luck that we here at WFL Weekly wish you, is good luck with injuries. Everything else is up to the higher powers.'}</p>
-                        <br></br>
-                        <p><strong>{'*Note from the Editor*:'}</strong>{' Big thank you to Andy & Drew on the WFL Weekly Website team, you guys rock.'}</p>
+
+                        <p style={{ fontStyle: "italic" }}>{'The only good luck that we here at WFL Weekly wish you, is good luck with injuries. Everything else is up to the higher powers.'}</p>
+
+                        <p><strong>{'*Note from the Editor*:'}</strong><em>{' Big thank you to Andy & Drew on the WFL Weekly Website team, you guys rock.'}</em></p>
                 </div>
             </div>
         </main>
