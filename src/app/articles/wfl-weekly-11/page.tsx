dfb75af84faa508b9f3fd975301134d80ab2e9d5
drewbloss <drew_bloss@CommishHQ.localdomain>
2025-11-16 19:03:27 -0500

    WFL Weekly 5 - Week 11

---
+++ src/app/articles/wfl-weekly-11/page.tsx
@@ -0,0 +1,135 @@
+import "../styles.css"
+
+export default function WFLWeekly11(){
+    return (
+        <main className = "min-h-screen bg-[#ffffff]">
+            <div className="header">
+                <h1 className="title-name">WFL WEEKLY 5 - Week 11: Burnout</h1>
+                <div className="post-meta">
+                    Papa Jack Media · Nov. 16
+                </div>
+                <div className="divider"></div>
+                <div className="article">
+                    <p>Going through some inspiration issues right now team. Just like Marvin&rsquo;s Room, we are beaten down and pretty tired. But also like that team, we are still fighting to get things done. We promise that better times and editions of WFL Weekly are ahead, we just have to wade through the mud to get there. To reach the rainbow you must endure the rain.</p>
+                    <p>Without further ado, let&rsquo;s rock and roll.</p>
+                    
+                    <h2>Biggest Winner:</h2>
+                    <p>Wake and Baker Co. (Jonah) - This guy just keeps on winning. In the WFL Game of the Week, he had star WR Garrett Wilson drop a donut&hellip; And still finished with 181 points. Moving to 7-3 and in firm control of 2nd place now, the decision he made against Johnny is looking smarter each and every day. Can he keep the good times rolling against a now 2-8 Cumming&hellip;?</p>
+
+                    <h2>Other Winners:</h2>
+                    <p>The Bang Bros (Andres) - Don&rsquo;t look now, but The Bang Bros aren&rsquo;t dead yet. Make that 2 wins in a row, as Andres and company picked up an impressive victory against Andy. The emergence (FINALLY) of TreVeyon Henderson, and RJ Harvey assuming RB1 duties in Denver could propel this team to new heights. A solid WR core now has the RB duo to match, and the original Bang Bro has quieted some of the noise that got especially loud during his losing streak. A win against a depleted I ❤️ Ohio might just force everyone else to take notice.</p>
+                    <p>Cumming&hellip; (Daniel) - Rare Daniel win! Woohoo!! This wasn&rsquo;t some fluke single-digit win either; Cumming&hellip; scored 150 points (good for 3rd highest) and blew the doors off of Build The Waller. Daniel saw only 2 of his players score less than 14 in a true team effort, and if trends like that continue, he could be a real thorn in the side of contending teams as the regular season wraps up.</p>
+
+                    <h2>Biggest Loser:</h2>
+                    <p>Build The Waller (Eric) - Well that&rsquo;s not ideal. Coming off the heels of the previous section, not reaching 100 points, getting blown out by 58 points, and the team doing the blowing out being Daniel, adds up tooooo&hellip; Not Good. A favorable matchup against Rico Suave at least gives Eric a chance to right the ship before it&rsquo;s too late. Dak &amp; Jake Ferguson will have to be big.</p>
+
+                    <h2>Other Losers:</h2>
+                    <p>I ❤️ Ohio (Ale) - It just keeps getting worse for Ale, who is hanging on to a playoff spot by a thread. A loss to Parker&rsquo;s team is nothing to be crazy concerned about, but nearly getting double your own points on your forehead is embarrassing. A mid-week trade for James Cook might be a nice bandaid, but we&rsquo;ll have to wait and see against a suddenly threatening Bang Bros team just how in trouble Ale really is.</p>
+                    <p>Worse than WHL (Jere) - Another week, another Monday Night Football loss. This time, by .28. While the gods certainly are not on Jere&rsquo;s side (lost the trade), the declining health of his players is concern #1 right now for Worse than WHL. What&rsquo;s ironic is his team in the WHL is doing worse &#x1f62c;</p>
+                    <p>FmFfms (Parker) - His name still don&rsquo;t make sense.</p>
+
+                    <h2>Donuts! (Sponsored by Tough Cookie Inc.):</h2>
+                    <p>The great donut boom may be slowly coming to an end, but that&rsquo;s doesn&rsquo;t mean our GMs aren&rsquo;t still craving some of those tasty treats.</p>
+                    <ul>
+                        <li>&#x1f369; DJ Moore (Build The Waller) - WFL Wars: Episode 5 - DJ Less Strikes Back</li>
+                        <li>&#x1f369; Garrett Wilson (Wake and Baker Co.) - The blind faith in Garrett Wilson will one day lead to one&rsquo;s demise, but Jonah&rsquo;s team might be too powerful for that right now. Bro probably just wants to sit out anyways, if the Jets offered me a contract today I&rsquo;d have to heavily weigh my options.</li>
+                        <li>&#x1f369; Dyami Brown (I ❤️ Ohio) - This guy shouldn&rsquo;t be on a team, much less this list. Come on Ale, there&rsquo;s better players out there than this.</li>
+                    </ul>
+
+                    <h2>Ale&rsquo;s Trade to Trade Analysis:</h2>
+                    <p><em>*Powered by WussiesGPT*</em></p>
+                    <p>No T2T this week :(</p>
+
+                    <h2>THE WFL HIERARCHY</h2>
+                    <p><em>*Proudly sponsored by Tough Cookie Inc. &amp; written by Drew*</em></p>
+                    <p>The WFL Hierarchy is proud to be sponsored by Tough Cookie Inc.&rsquo;s NEW Crack, Don&rsquo;t Crumble Series! Don&rsquo;t forget to check-in every week, we&rsquo;ll be rolling out new items throughout the season.</p>
+                    <p>Featured Cookie of the Week: The Bang Bros Blondies</p>
+
+                    <h3>Rankings:</h3>
+                    <ul>
+                        <li>1. Wake and Baker Co.</li>
+                        <li>2. Qui Gyatt Jit</li>
+                        <li>3. Forgive me Father for my sins</li>
+                        <li>4. Worse than WHL</li>
+                        <li>5. KRAFT YAC &amp; CHEESE</li>
+                        <li>6. Hall of Fame Youtuber</li>
+                        <li>7. Marvin&rsquo;s Room</li>
+                        <li>8. Rico Suave</li>
+                        <li>9. I ❤️ Ohio</li>
+                        <li>10. The Bang Bros</li>
+                        <li>11. Build the Waller</li>
+                        <li>12. Cumming&hellip;</li>
+                    </ul>
+
+                    <p>Not a ton of changes in the WFL Hierarchy this week, but we still have a few movers.</p>
+
+                    <h3>Risers:</h3>
+                    <p>KRAFT YAC &amp; CHEESE - One week after being in our fallers section, Kraft YAC &amp; Cheese has found themselves on the opposite end of our list. We thought that the loss of Tucker Kraft would be too heavy a burden for this team to bear, but they handled it swimmingly. Dalton Schultz proved a relatively fine replacement, and Drew went out to acquire yet another Top 20 WR in Tee Higgins. This team could continue to move higher once Omarion Hampton returns, but for now this WR room moves them up 2 spots to number 5.</p>
+
+                    <h3>Fallers:</h3>
+                    <p>Marvin&rsquo;s Room - Another week, another injury for Marvin&rsquo;s Room. It&rsquo;s honestly getting to the point where we might consider consulting the history books for total number of injured players for a single team. And this one is the most insulting of them all. Appendicitis? Really? Losing your team captain and namesake is never easy, especially to a non-football injury. That was the last thing Jack needed heading into a must-win matchup with Andy this week and he moves down 2 spots to number 7.</p>
+
+                    <h3>Other Adjustments:</h3>
+                    <p>Eric and Andre&rsquo;s swap positions at 10 &amp; 11 after JK Dobbins injury makes Harvey a mid-range RB2.</p>
+
+                    <h2>Papa&rsquo;s Picks:</h2>
+
+                    <h3>WFL Weekly Game of the Week: I ❤️ Ohio (Ale) vs. The Bang Bros (Andres)</h3>
+                    <p>This week&rsquo;s WFL Weekly Game of the Week features two teams going in opposite directions. The floundering I ❤️ Ohio may have lost all its plot armor, but they still sit in a playoff spot as it stands. The Bang Bros have had an up and down season to put it lightly, but 2 wins in a row have sparked some optimism that a return to the playoffs might just be in reach. Ale will hope that James Cook and Troy Franklin can reverse his fortunes, while Andres is banking on breakout rookies TreVeyon Henderson and RJ Harvey to shine in expanded roles. Will The Bang Bros get banged? Will I ❤️ Ohio&rsquo;s tumble down the standings continue? It&rsquo;s anyone&rsquo;s guess in a pivotal Week 11 matchup.</p>
+
+                    <h3>Upset Watch of the Week:</h3>
+                    <p>(Same as above)</p>
+
+                    <h3>Predictions: (Last Week&rsquo;s results: 5-1 &#x1f60e; 38-22 overall)</h3>
+
+                    <ul>
+                        <li>#2 Wake and Baker Co. (Jonah) &#x2705; vs. #12 Cumming&hellip; (Daniel)</li>
+                        <li>#8 Marvin&rsquo;s Room (Jack) vs. #3 Qui Gyatt Jit (Andy) &#x2705;</li>
+                        <li>#1 KRAFT YAC &amp; CHEESE (Drew) &#x2705; vs. #6 Worse than WHL (Jere)</li>
+                        <li>#10 Build the Waller (Eric) &#x2705; vs. #11 Rico Suave (Johny)</li>
+                        <li>#7 Hall of Fame Youtuber (Fab) &#x2705; vs. #5 FmFfms (Parker)</li>
+                        <li>#4 I ❤️ Ohio (Ale) vs. #9 The Bang Bros (Andres) &#x2705;</li>
+                    </ul>
+
+                    <h3>Divisional Standings(Divisional Record in parentheses)</h3>
+
+                    <p><strong><u>Cummish Division</u></strong></p>
+                    <ul>
+                        <li>#1 KRAFT YAC &amp; CHEESE (Drew): 8-2 (1-1)</li>
+                        <li>#3 yakub (Andy): 6-4 (2-0)</li>
+                        <li>#12 Cumming&hellip; (Daniel): 2-8 (0-2)</li>
+                    </ul>
+
+                    <p><strong><u>33176+ Division</u></strong></p>
+                    <ul>
+                        <li>#2 Wake and Baker Co. (Jonah): 7-3 (1-1)</li>
+                        <li>#8 Marvin&apos;s Room (Jack): 4-6 (1-1)</li>
+                        <li>#9 The Bang Bros (Andres): 4-6 (1-1)</li>
+                    </ul>
+
+                    <p><strong><u>sybau Division</u></strong></p>
+                    <ul>
+                        <li>#4 I ❤️ Ohio (Alejandro): 6-4 (2-0)</li>
+                        <li>#7 Hall of Fame Youtuber (Fabian): 5-5 (0-2)</li>
+                        <li>#10 Build The Waller (Eric): 4-6 (1-1)</li>
+                    </ul>
+                    <p><strong><u>Trade2Trade Division</u></strong></p>
+                    <ul>
+                        <li>#5 Rome wasn&rsquo;t built in a day (Parker): 5-5 (2-0)</li>
+                        <li>#6 Worse than WHL (Jeremias): 5-5 (1-1)</li>
+                        <li>#10 Rico Suave (Johnny): 4-6 (0-2)</li>
+                    </ul>
+                    <p>A big shoutout to our sponsors Tough Cookie Inc. and WussiesGPT for helping the Donut section &amp; Trade to Trade be what they are today, and to Drew for his power rankings!</p>
+                    <p><br></br></p>
+                    <p>If you or someone you know would like to be a sponsor for WFL Weekly, let us know and we&rsquo;ll give you some options on where you can showcase your brand, company, or organization!</p>
+                    <p><br></br></p>
+                    <p>Additionally, if you have your own ideas for columns, one offs, or weekly installments, speak to Chief Editor Jack Reid for more information!</p>
+                    <p><br></br></p>
+                    <p>The only good luck that we here at WFL Weekly wish you, is good luck with injuries. Everything else is up to the higher powers.</p>
+                    <p><br></br></p>
+                    <p><em>*<strong>Note from the Editor*:&nbsp;</strong>Big thank you to Andy &amp; Drew on the WFL Weekly Website team, you guys rock.</em></p>
+                </div>
+            </div>
+        </main>
+    )
+}
